d9e26b277c8c829caebfcc92bc69ec36f23c156d
Khoa Nguyen <khoa01004@gmail.com>
2021-01-28 18:04:55 -0500

    Final

--- WiFiDiagnosis.java
+++ WiFiDiagnosis.java
@@ -1,7 +1,7 @@
 /*
  * Class: CMSC203 
  * Instructor: Khandan Vahabzadeh Monshi
- * Description: the WiFiDiagnosis class use to run the main program
+ * Description: Create a WiFiDiagnosis program to diagnosis wifi
  * Due: 2/02/2021
  * Platform/compiler: Eclipse
  * I pledge that I have completed the programming assignment independently.
@@ -10,7 +10,7 @@
    Print your Name here: Kevin Nguyen
 */
 
-                                                //Should not use string in this assignment
+                                                //Should not use array in this assignment
 import java.util.Scanner;
 
 public class WiFiDiagnosis
@@ -18,31 +18,73 @@ public class WiFiDiagnosis
     public static void main(String[] args)
     {
         //These variables contain prompts that could ask the user to diagnostic wifi
-        String a = "\nAre you able to connect with the internet? (yes or no)\n";
-        String[] b ={"First step: reboot your computer", 
-                    "Second step: reboot your router",
-                    "Third step: make sure the cables to your router are plugged in firmly and your router is getting power",
-                    "Forth step: Move the computer closer to the router and try to reconnect",
-                    "Try to contact your ISP"};
+        String first = "First step: reboot your computer";
+        String second = "Second step: reboot your router";
+        String third = "Third step: make sure the cables to your router are plugged in firmly and your router is getting power";
+        String forth = "Forth step: Move the computer closer to the router and try to reconnect";
+        String last = "Try to contact your ISP";
+        String check = "Are you able to connect with the internet? (yes or no)";
+        String worked = " seems to work";
+        String thanks = "Thanks for using!!!";
 
         Scanner reader = new Scanner(System.in);
         System.out.println("This is a Wifi diagnostic program, this program might help with your wifi problem");
+        System.out.println(first);
+        System.out.println(check);
 
         int i =0;
         for (; i<=3 ; i++)
         {
-            System.out.print(b[i]+a);
             String input = reader.nextLine();
-            if (input.equals("yes"))
-            {
-                System.out.print(b[i]+" seems to work");
+            if (input.equals("yes") && i ==0)
+            {   
+                System.out.println(first + worked);
+                System.out.println (thanks);
                 break;
             }
-        }
-        //This is when the internet is still not working after tries many steps
-        if (i ==4)
-        {
-            System.out.print(b[i]);
+            else if (input.equals("yes") && i ==1)
+            {
+                System.out.println(second + worked);
+                System.out.println (thanks);
+                break;
+            }
+            else if (input.equals("yes") && i ==2)
+            {
+                System.out.println(third + worked);
+                System.out.println (thanks);
+                break;
+            }
+            else if (input.equals("yes") && i ==3)
+            {
+                System.out.println(forth + worked);
+                System.out.println (thanks);
+                break;
+            }
+            else if(input.equals("no") && i==0)
+            {
+                System.out.println(second);
+                System.out.println(check);
+            }
+            else if (input.equals("no") && i == 1)
+            {
+                System.out.println(third);
+                System.out.println(check);
+            }
+            else if (input.equals("no") && i == 2)
+            {
+                System.out.println(forth);
+                System.out.println(check);
+            }
+            else if (input.equals("no") && i ==3)     //This is when the internet is still not working after tries many steps
+            {
+                System.out.println(last);
+                System.out.println (thanks);
+            }
+            else                                      //This is when user type in something weird
+            {
+                System.out.println("I don't understand please type again");
+                i-=1;
+            }
         }
         reader.close();
     }
